cb1c5d03cbb741180d1985fe4c3652eb0e24da19
Revan Faredha <revanfaredha@gmail.com>
2019-10-23 12:40:49 +0700

    server prod ready

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/nodeb';
+    protected $redirectTo = '/home';
     protected $username = 'username';
 
     /**
